9dcf73e4e20a2f89b8c606a667f02886c466b286
ValdasJo <valdasj94@gmail.com>
2025-11-18 05:35:46 +0200

    Building user interface

---
+++ client/src/core/pipes/age-pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'age'
+})
+export class AgePipe implements PipeTransform {
+
+  transform(value: string): number {
+    const today = new Date();
+    const dateOfbirth = new Date(value);
+    
+    let age = today.getFullYear() - dateOfbirth.getFullYear();
+    const monthDiff = today.getMonth() - dateOfbirth.getMonth();
+
+    if(monthDiff < 0 || (monthDiff === 0 && today.getDate() < dateOfbirth.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+
+}
