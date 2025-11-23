a976778b54dd9017edcc9959dd3da1e9cef8da6a
ValdasJo <valdasj94@gmail.com>
2025-11-23 20:28:33 +0200

    Reactive forms

---
+++ client/src/shared/text-input/text-input.ts
@@ -0,0 +1,30 @@
+import { Component, input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './text-input.html',
+  styleUrl: './text-input.css',
+})
+export class TextInput implements ControlValueAccessor {
+  label = input<string>('');
+  type = input<string>('text');
+  maxDate = input<string>('');
+
+  constructor(@Self() public ngCongrol: NgControl) {
+    this.ngCongrol.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+  }
+  registerOnChange(fn: any): void {
+  }
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    return this.ngCongrol.control as FormControl;
+  }
+
+}
